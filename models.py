e140548aede151333a70cfd54dc123bba9b5ff9e
CyberFusion <168574767+Mootube10@users.noreply.github.com>
2025-04-22 17:35:10 +0100

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)  # New field
-    points = Column(Integer, default=0)
