ef222370a3fa9e943e34724af303e2fdd5777e06
CyberFusion <168574767+Mootube10@users.noreply.github.com>
2025-04-22 17:19:32 +0100

    Update crud.py

--- crud.py
+++ crud.py
@@ -1,21 +1,18 @@
-from sqlalchemy.orm import Session
-from models import User
-from schemas import UserCreate
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 def create_user(db: Session, user: UserCreate):
-    db_user = User(username=user.username, email=user.email)
+    hashed_password = get_password_hash(user.password)
+    db_user = User(
+        username=user.username,
+        email=user.email,
+        password=hashed_password
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return db_user
-
-def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
-
-def add_points(db: Session, user_id: int, points: int):
-    db_user = db.query(User).filter(User.id == user_id).first()
-    if db_user:
-        db_user.points += points
-        db.commit()
-        db.refresh(db_user)
     return db_user
